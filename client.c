f05a2fe2a3ff1427933417a457eac440973231b4
allancrabelo <allanaugustocarneirorabelo@gmail.com>
2025-07-26 16:01:16 +0100

    added: client finished.

--- client.c
+++ client.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaugusto <aaugusto@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: aaugusto <<aaugusto@student.42porto.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 10:17:16 by aaugusto          #+#    #+#             */
-/*   Updated: 2025/07/26 10:45:40 by aaugusto         ###   ########.fr       */
+/*   Updated: 2025/07/26 15:56:19 by aaugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,28 @@ void	send_size(int pid, int size)
 	}
 }
 
+void	send_message(int pid, char *str, int size)
+{
+	int	i;
+	int	x;
+
+	i = 0;
+	while(i < size)
+	{
+		x = 8;
+		while (x > 0)
+		{
+			if (str[i] & 0b10000000)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			x--;
+			usleep(100);
+		}
+		i++;	
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int		pid;
@@ -39,7 +61,7 @@ int	main(int argc, char **argv)
 	pid = ft_atoi(argv[1]);
 	str = argv[2];
 	len = ft_strlen(str);
-	send_message(pid, len);
-	
+	send_size(pid, len);
+	send_message(pid, str, len);
 	return (0);
 }
